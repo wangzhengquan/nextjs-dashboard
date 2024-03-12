aefb72ff9e73d7fc161f706d8d3ae8845f1a43e5
wangzhengquan <wangzhengquan85@126.com>
2024-03-12 19:51:00 +0800

    update

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,6 +9,7 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import {revenue} from './placeholder-data'
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
@@ -21,11 +22,12 @@ export async function fetchRevenue() {
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const data = await sql<Revenue>`SELECT * FROM revenue`;
+    // const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     // console.log('Data fetch completed after 3 seconds.');
 
-    return data.rows;
+    // return data.rows;
+    return revenue
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
